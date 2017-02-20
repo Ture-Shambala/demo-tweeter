61dad03fcb164ba4b1bcb6707191dd676ba770f2
ture-shambala <tureshambala4@gmail.com>
2017-02-20 08:27:33 +0530

    Created user with devise gem

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+	def index
+		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def show
+		
+	end
+
+	def create
+		binding.pry
+		@user = User.new
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+end
